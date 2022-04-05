3e9d9ffc6d911e5b848a46e50e71971ab57a689b
DmitriyGera <dimmo12397@mail.ru>
2022-04-05 15:43:12 +0300

    Домашнее задание

---
+++ fork001/Program.cs
@@ -0,0 +1,28 @@
+﻿double[,] array(int m, int n)
+{
+    double[,] value = new double[m, n];
+    for (int i = 0; i < value.GetLength(0); i++)
+    {
+        for (int j = 0; j < value.GetLength(1); j++)
+        {
+            value[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return value;
+}
+
+void PrintArray(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            Console.Write(tab[i, j] + "    ");
+        }
+    }
+}
+
+int m = 4;
+int n = 2;
+double[,] number = array(m, n);
+PrintArray(number);
---
+++ fork002/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+int find = 3; //Число которое ищем.
+int n = 0;
+bool f = false;
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (find == arr[i, j]) {n = j; f = true; };
+        Console.Write("{0}\t", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+if (f)
+    Console.WriteLine($"Число найдено: столбец {n + 1} ");
+else
+    Console.WriteLine("Число не найдено");
+
